2a7916c7057f10b5ebb4a923ddf433dd8bb7c2e0
jakubstandarski <jakub.standarski@gmail.com>
2022-01-21 16:37:23 +0100

    feat(embedded_device): add empty circular_buffer.h

---
+++ embedded_device/firmware/services/include/circular_buffer.h
@@ -0,0 +1,14 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 21.01.2022
+ */
+
+#ifndef CIRCULAR_BUFFER_H
+    #define CIRCULAR_BUFFER_H
+
+
+
+#endif /* CIRCULAR_BUFFER_H */
+
