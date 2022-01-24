84fab0ccd7a3d13ee87593a3f3a667d874434588
jakubstandarski <jakub.standarski@gmail.com>
2022-01-24 16:32:32 +0100

    feat(embedded_device): define circular_buffer private structure

--- embedded_device/firmware/services/source/circular_buffer.c
+++ embedded_device/firmware/services/source/circular_buffer.c
@@ -18,3 +18,29 @@
 
 
 
+/*****************************************************************************/
+/* PRIVATE MACROS */
+/*****************************************************************************/
+
+#define CIRCULAR_BUFFER_AVAILABLE_COUNT ((size_t)4)
+
+#define DATA_BUFFER_SIZE_MAX    ((size_t)256)
+
+
+
+/*****************************************************************************/
+/* PRIVATE STRUCTURES */
+/*****************************************************************************/
+
+struct circular_buffer {
+    bool assigned;
+    uint8_t data_buffer[DATA_BUFFER_SIZE_MAX];
+    size_t data_count;
+    size_t head;
+    size_t tail;
+};
+
+static struct circular_buffer circular_buffers[CIRCULAR_BUFFER_AVAILABLE_COUNT];
+
+
+
