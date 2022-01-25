89f79391d73f63771aa370b8f7fb742c83c36d31
jakubstandarski <jakub.standarski@gmail.com>
2022-01-25 17:53:38 +0100

    feat(embedded_device): add header files in debug.c

--- embedded_device/firmware/services/source/debug.c
+++ embedded_device/firmware/services/source/debug.c
@@ -5,3 +5,23 @@
  * Date: 21.01.2022
  */
 
+/*****************************************************************************/
+/* HEADER FILES */
+/*****************************************************************************/
+
+#include "debug.h"
+#include "circular_buffer.h"
+
+#include "stm32l4xx_ll_bus.h"
+#include "stm32l4xx_ll_gpio.h"
+#include "stm32l4xx_ll_usart.h"
+
+#include "stm32l432xx.h"
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
