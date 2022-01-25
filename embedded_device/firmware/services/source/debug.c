154f07f142a52d69fae9b958fdb692bc4fe7d5ba
jakubstandarski <jakub.standarski@gmail.com>
2022-01-25 18:01:24 +0100

    feat(embedded_device): add debug_uart_peripheral_init() helper function

--- embedded_device/firmware/services/source/debug.c
+++ embedded_device/firmware/services/source/debug.c
@@ -47,6 +47,10 @@
 
 static void debug_uart_gpio_init(void);
 
+static void debug_uart_peripheral_init(void);
+
+
+
 
 
 /*****************************************************************************/
@@ -68,6 +72,29 @@ static void debug_uart_gpio_init(void)
         .Alternate = DEBUG_UART_GPIO_PIN_ALTERNATE_FUNCTION
     };
     LL_GPIO_Init(DEBUG_UART_GPIO_PORT, &uart_gpio_config);
+}
+
+
+
+static void debug_uart_peripheral_init(void)
+{
+    if (LL_APB2_GRP1_IsEnabledClock(DEBUG_UART_PERIPHERAL_CLOCK) == 0) {
+        LL_APB2_GRP1_EnableClock(DEBUG_UART_PERIPHERAL_CLOCK);
+    }
+
+    LL_USART_InitTypeDef uart_peripheral_config = {
+        .BaudRate = DEBUG_UART_PERIPHERAL_BAUDRATE,
+        .DataWidth = LL_USART_DATAWIDTH_8B,
+        .StopBits = LL_USART_STOPBITS_1,
+        .Parity = LL_USART_PARITY_NONE,
+        .TransferDirection = LL_USART_DIRECTION_TX,
+        .HardwareFlowControl = LL_USART_HWCONTROL_NONE,
+        .OverSampling = LL_USART_OVERSAMPLING_16
+    };
+    LL_USART_Init(DEBUG_UART_PERIPHERAL_PORT, &uart_peripheral_config);
+
+    LL_USART_Enable(DEBUG_UART_PERIPHERAL_PORT);
+    NVIC_EnableIRQ(DEBUG_UART_PERIPHERAL_IRQ);
 }
 
 
