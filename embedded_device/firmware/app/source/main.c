394c950150c9d7f8c0f048ea5a10ce5c02bccb1b
jakubstandarski <jakub.standarski@gmail.com>
2022-01-26 13:52:01 +0100

    feat(embedded_device): add tick count logger task to test debug functionality

--- embedded_device/firmware/app/source/main.c
+++ embedded_device/firmware/app/source/main.c
@@ -10,6 +10,7 @@
 /* HEADER FILES */
 /*****************************************************************************/
 
+#include "debug.h"
 #include "led.h"
 #include "system.h"
 
@@ -26,6 +27,8 @@
 
 static void blink_led(void *parameter);
 
+static void tick_count_logger(void *parameter);
+
 
 
 /*****************************************************************************/
@@ -35,15 +38,17 @@ static void blink_led(void *parameter);
 int main(void)
 {
     system_init();
+    debug_init();
     led_init(LED_PIN_BOARD_GREEN_LD3);
 
-    BaseType_t return_code = xTaskCreate(blink_led, "blink_led",
+    debug_printf("Hello from STM32L432xx!\n");
+
+    xTaskCreate(blink_led, "blink_led", configMINIMAL_STACK_SIZE, NULL, 1,
+        NULL);
+    xTaskCreate(tick_count_logger, "tick_count_logger",
         configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    if (return_code == pdPASS) {
-        vTaskStartScheduler();
-    } else {
-        ;
-    }
+
+    vTaskStartScheduler();
 
     while (1) {
         ;
@@ -63,6 +68,18 @@ static void blink_led(void *parameter)
     while (1) {
         led_toggle(LED_PIN_BOARD_GREEN_LD3);
         vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+}
+
+
+
+static void tick_count_logger(void *parameter)
+{
+    while (1) {
+        TickType_t tick_count = xTaskGetTickCount();
+        debug_printf("Tick count since scheduler has started: %u\n",
+            tick_count);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
