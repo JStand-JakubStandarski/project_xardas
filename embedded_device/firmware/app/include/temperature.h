1af446ea9ad79f07b239211ea47e72b44d7b3e66
jakubstandarski <jakub.standarski@gmail.com>
2022-02-03 13:30:54 +0100

    feat(embedded_device): add empty temperature.h file

---
+++ embedded_device/firmware/app/include/temperature.h
@@ -0,0 +1,14 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 03.02.2022
+ */
+
+#ifndef TEMPERATURE_H
+    #define TEMPERATURE_H
+
+
+
+#endif /* TEMPERATURE_H */
+
