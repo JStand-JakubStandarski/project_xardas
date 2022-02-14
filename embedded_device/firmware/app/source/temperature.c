a75ca47913a71f7dcaee42deac2f385485835072
jakubstandarski <jakub.standarski@gmail.com>
2022-02-14 15:56:34 +0100

    feat(embedded_device): add header files in temperature.c

--- embedded_device/firmware/app/source/temperature.c
+++ embedded_device/firmware/app/source/temperature.c
@@ -5,3 +5,25 @@
  * Date: 03.02.2022
  */
 
+/*****************************************************************************/
+/* HEADER FILES */
+/*****************************************************************************/
+
+#include "temperature.h"
+
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+
+#include "stm32l4xx_ll_adc.h"
+#include "stm32l4xx_ll_bus.h"
+#include "stm32l4xx_ll_gpio.h"
+#include "stm32l4xx_ll_tim.h"
+
+#include "stm32l432xx.h"
+
+#include <limits.h>
+#include <math.h>
+
+
+
