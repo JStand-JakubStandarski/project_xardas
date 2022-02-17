441dc54dab1578f46f9064257044eb370de4a4f3
jakubstandarski <jakub.standarski@gmail.com>
2022-02-17 16:25:05 +0100

    feat(embedded_device): update header files

--- embedded_device/firmware/services/source/debug.c
+++ embedded_device/firmware/services/source/debug.c
@@ -10,7 +10,10 @@
 /*****************************************************************************/
 
 #include "debug.h"
-#include "circular_buffer.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 
 #include "stm32l4xx_ll_bus.h"
 #include "stm32l4xx_ll_gpio.h"
@@ -20,6 +23,7 @@
 
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
