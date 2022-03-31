8c41d066f258aae5ed4fc26c3b32fd71b4832e33
jakubstandarski <jakub.standarski@gmail.com>
2022-03-31 16:32:01 +0200

    feat(embedded_device): add enable_rtc_init_mode() helper function

--- embedded_device/firmware/app/source/rtc.c
+++ embedded_device/firmware/app/source/rtc.c
@@ -47,3 +47,35 @@ static QueueHandle_t rtc_date_queue = NULL;
 
 
 
+/*****************************************************************************/
+/* PRIVATE HELPER FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+static void enable_rtc_init_mode(void);
+
+
+
+/*****************************************************************************/
+/* PRIVATE HELPER FUNCTIONS DEFINITIONS */
+/*****************************************************************************/
+
+static void enable_rtc_init_mode(void)
+{
+    LL_RTC_DisableWriteProtection(RTC);
+
+    bool rtc_init_mode_off = !LL_RTC_IsActiveFlag_INIT(RTC);
+    if (rtc_init_mode_off) {
+
+        LL_RTC_EnableInitMode(RTC);
+
+        rtc_init_mode_off = !LL_RTC_IsActiveFlag_INIT(RTC);
+        while (rtc_init_mode_off) {
+
+            vTaskDelay(pdMS_TO_TICKS(1));
+            rtc_init_mode_off = !LL_RTC_IsActiveFlag_INIT(RTC);
+        }
+    }
+}
+
+
+
