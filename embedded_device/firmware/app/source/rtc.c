c5dd54ce6199163dff17ff996b4e8ab18ad6bda8
jakubstandarski <jakub.standarski@gmail.com>
2022-03-31 16:27:58 +0200

    feat(embedded_device): add required header files in rtc.c

--- embedded_device/firmware/app/source/rtc.c
+++ embedded_device/firmware/app/source/rtc.c
@@ -5,3 +5,25 @@
  * Date: 08.03.2022
  */
 
+/*****************************************************************************/
+/* HEADER FILES */
+/*****************************************************************************/
+
+#include "debug.h"
+#include "rtc.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#include "stm32l4xx_ll_bus.h"
+#include "stm32l4xx_ll_rcc.h"
+#include "stm32l4xx_ll_rtc.h"
+
+#include "stm32l432xx.h"
+
+#include <stdbool.h>
+#include <time.h>
+
+
+
