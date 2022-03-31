f017b7a8d41d2b4722d20995dde53d433995179a
jakubstandarski <jakub.standarski@gmail.com>
2022-03-31 16:36:36 +0200

    feat(embedded_device): add public enum rtc_month

--- embedded_device/firmware/app/include/rtc.h
+++ embedded_device/firmware/app/include/rtc.h
@@ -8,6 +8,27 @@
 #ifndef RTC_H
     #define RTC_H
 
+/*****************************************************************************/
+/* PUBLIC ENUMS */
+/*****************************************************************************/
+
+enum rtc_month {
+    RTC_MONTH_JANUARY = 1,
+    RTC_MONTH_FEBRUARY,
+    RTC_MONTH_MARCH,
+    RTC_MONTH_APRIL,
+    RTC_MONTH_MAY,
+    RTC_MONTH_JUNE,
+    RTC_MONTH_JULY,
+    RTC_MONTH_AUGUST,
+    RTC_MONTH_SEPTEMBER,
+    RTC_MONTH_OCTOBER,
+    RTC_MONTH_NOVEMBER,
+    RTC_MONTH_DECEMBER
+};
+
+
+
 
 
 #endif /* RTC_H */
